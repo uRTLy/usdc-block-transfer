32c97650bc5a2f8fe744f02dcdf8f9e6cb4d268e
Patrick Tluszcz <17055613+uRTLy@users.noreply.github.com>
2026-06-24 18:43:35 +0200

    registered asset registry, token providers, added missing blockchain evm import

--- apps/usdc-block-transfers/src/app.module.ts
+++ apps/usdc-block-transfers/src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER } from '@nestjs/core';
-import { ASSET_REGISTRY } from '@app/application';
-import { BlockchainEvmModule } from '@app/blockchain-evm';
+import {
+  ASSET_REGISTRY,
+  GetTransfersUseCase,
+  TOKEN_TRANSFER_PROVIDER_REGISTRY,
+  TOKEN_TRANSFER_PROVIDERS,
+  type AssetRegistryPort,
+  type TokenTransferProviderPort,
+  type TokenTransferProviderRegistryPort,
+} from '@app/application';
+import {
+  BlockchainEvmModule,
+  EVM_TOKEN_TRANSFER_PROVIDER,
+} from '@app/blockchain-evm';
 import { mainnet } from 'viem/chains';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -37,6 +48,32 @@ import { TransfersController } from './transfers/transfers.controller';
           chains: SUPPORTED_CHAINS,
           assets: SUPPORTED_ASSETS,
         }),
+    },
+    {
+      provide: TOKEN_TRANSFER_PROVIDERS,
+      useFactory: (
+        evmProvider: TokenTransferProviderPort,
+      ): TokenTransferProviderPort[] => [evmProvider],
+      inject: [EVM_TOKEN_TRANSFER_PROVIDER],
+    },
+    {
+      provide: TOKEN_TRANSFER_PROVIDER_REGISTRY,
+      useFactory: (
+        providers: TokenTransferProviderPort[],
+      ): TokenTransferProviderRegistryPort => ({
+        getProvider: ({ chain, asset }) =>
+          providers.find((provider) => provider.supports({ chain, asset })) ??
+          null,
+      }),
+      inject: [TOKEN_TRANSFER_PROVIDERS],
+    },
+    {
+      provide: GetTransfersUseCase,
+      useFactory: (
+        assetRegistry: AssetRegistryPort,
+        providerRegistry: TokenTransferProviderRegistryPort,
+      ) => new GetTransfersUseCase(assetRegistry, providerRegistry),
+      inject: [ASSET_REGISTRY, TOKEN_TRANSFER_PROVIDER_REGISTRY],
     },
     {
       provide: APP_FILTER,
