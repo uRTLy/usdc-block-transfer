729ca42a79bc5631ec184aaee1e7c11b6c56235b
Patrick Tluszcz <17055613+uRTLy@users.noreply.github.com>
2026-06-24 16:36:32 +0200

    added module name mapper, fixed import

--- libs/application/src/use-cases/get-transfers.use-case.spec.ts
+++ libs/application/src/use-cases/get-transfers.use-case.spec.ts
@@ -1,4 +1,4 @@
-import { Asset, Chain, Transfer } from '../../../domain/src';
+import { Asset, Chain, Transfer } from '@app/domain';
 import { ApplicationError } from '../errors/application-error';
 import { AssetRegistryPort } from '../ports/asset-registry.port';
 import { TokenTransferProviderRegistryPort } from '../ports/token-transfer-provider-registry.port';
