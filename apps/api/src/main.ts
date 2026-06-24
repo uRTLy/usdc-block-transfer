d3573139db37cee2d3d2740b66a8c24275ae9e90
Patrick Tluszcz <17055613+uRTLy@users.noreply.github.com>
2026-06-24 13:36:56 +0200

    removed apps/api

--- apps/api/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { ApiModule } from './api.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(ApiModule);
-  await app.listen(process.env.port ?? 3000);
-}
-bootstrap();
